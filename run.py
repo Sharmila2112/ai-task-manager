881c6a3903792023f9679ee5fefaf8c6aa0c21f0
A. Sharmila <sharmila949231@gmail.com>
2026-07-07 22:44:15 +0530

    Auto create database tables on startup

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-from app import create_app
+from dotenv import load_dotenv
+from app import create_app, db
+
+# Load environment variables
+load_dotenv()
 
 app = create_app()
+
+# Create database tables if they don't exist
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
